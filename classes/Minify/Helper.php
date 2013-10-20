8e7983b4d8dd5b227cfee3e114b867fd96d72126
Drew J. Sonne <drew.sonne@gmail.com>
2013-10-20 21:31:38 +1100

    Updated minify

---
+++ classes/Minify/Helper.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: "Drew J. Sonne" <drews@symbiosislabs.com>
+ * Date: 20/10/13
+ * Time: 10:51 AM
+ * Package: net.symbiosislabs.tech
+ */
+
+class Minify_Helper
+{
+
+    public static function js($group)
+    {
+        return self::_output('script', $group);
+    }
+
+    public static function css($group)
+    {
+        return self::_output('style', $group);
+    }
+
+    protected static function _output($type, $group)
+    {
+        extract(Kohana::$config->load('minify')->as_array(), EXTR_SKIP);
+
+        $debug = array_key_exists('debug', $_GET) && $allowDebugFlag;
+
+        if ($debug) {
+            $group = $groupsConfig[$group];
+            return array_reduce($group, function ($result, $object) use ($type) {
+                $result .= html::$type($object) . "\n";
+                echo($result);
+            }, '');
+        } else {
+            return html::$type("min/{$group}");
+        }
+    }
+
+}
